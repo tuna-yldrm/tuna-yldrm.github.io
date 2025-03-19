b6e5c6bf89b2ad19fcc87428b10cff055ce3dfa8
Tunahan Yıldırım <tunahan19yildirim@gmail.com>
2025-03-19 11:04:40 +0300

    Add files via upload

--- HW1-Q2.cpp
+++ HW1-Q2.cpp
@@ -8,7 +8,7 @@ int main( void )
 {
    int integer1; /* first number to be input by user  */
    int integer2 ;/* second number to be input by user */
-   int x;      /* variable in which sum will be stored */ 
+   int sum;      /* variable in which sum will be stored */ 
 
    printf( "Enter first integer\n" ); /* prompt */
    scanf( "%d", &integer1 );        /* read an integer */
@@ -16,7 +16,7 @@ int main( void )
    printf( "Enter second integer\n" ); /* prompt */
    scanf( "%d", &integer2 );           /* read an integer */
    
-   int sum = integer1 + integer2; /* assign total to sum */
+   sum = integer1 + integer2; /* assign total to sum */
 
    printf( "Sum is %d\n", sum ); /* print sum */
    return 0; /* indicate that program ended successfully */
